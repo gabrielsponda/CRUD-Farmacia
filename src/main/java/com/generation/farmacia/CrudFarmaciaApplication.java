4ce82cc047f6d9876c7a73e05fe7dc3100d3cde5
gabrielsponda <spondagabriel@gmail.com>
2024-01-05 01:57:07 -0300

    Implementando categoria.

--- src/main/java/com/generation/farmacia/CrudFarmaciaApplication.java
+++ src/main/java/com/generation/farmacia/CrudFarmaciaApplication.java
@@ -1,4 +1,4 @@
-package com.generation.quitanda;
+package com.generation.farmacia;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
---
+++ src/main/java/com/generation/farmacia/model/Categoria.java
@@ -0,0 +1,39 @@
+package com.generation.farmacia.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+@Entity
+@Table(name = "tb_categorias")
+public class Categoria {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@NotBlank(message = "A categoria deve possuir uma descrição.")
+	@Size(max = 42, message = "A categoria deve ser descrita de forma breve, com no máximo 42 caracteres.")
+	private String descricao;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+	
+}
